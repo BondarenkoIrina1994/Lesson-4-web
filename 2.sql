9201fbbd116c6cdea14f2e206a3a333f5314fe66
Irina <bondarenko-irinka@mail.ru>
2023-03-21 09:42:49 +0300

    Homework4

---
+++ 2.sql
@@ -0,0 +1,18 @@
+-- create
+CREATE TABLE GROUP (
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+age INTEGER NOT NULL,
+address TEXT NOT NULL
+);
+-- insert
+INSERT INTO GROUP VALUES (1, 'Ирина', 28, 'Ростов-на-Дону');
+INSERT INTO GROUP VALUES (2, 'Алена', 28, 'Ростов-на-Дону');
+INSERT INTO GROUP VALUES (3, 'Максим', 33, 'Ростов-на-Дону');
+INSERT INTO GROUP VALUES (4, 'Егор', 32, 'Новочеркасск');
+INSERT INTO GROUP VALUES (5, 'Николай', 25, 'Москва');
+INSERT INTO GROUP VALUES (6, 'Иван', 23, 'Санкт-Петербург');
+INSERT INTO GROUP VALUES (7, 'Андрей', 25, 'Волгоград');
+INSERT INTO GROUP VALUES (8, 'Александр', 25, 'Самара');
+INSERT INTO GROUP VALUES (9, 'Анна', 22, 'Москва');
+INSERT INTO GROUP VALUES (10, 'Ирина', 20, 'Нижний Новгород');
